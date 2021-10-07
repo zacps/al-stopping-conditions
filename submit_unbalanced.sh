d967bad73eaebd13e95d06ab1a4e90402efdb8eb
Zac Pullar-Strecker <zacmps@gmail.com>
2021-10-07 08:11:33 +0000

    Remove old notebooks

--- submit_unbalanced.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#poetry run python slurm.py nesi_unbalanced.py avila-unbalanced 0 1 0-1 --mem 612M --time 0-00:02:00
-poetry run python slurm.py nesi_unbalanced.py sensorless-unbalanced 1 1 0-1 --mem 612M --time 1-00:00:00
-#poetry run python slurm.py nesi_unbalanced.py anuran-unbalanced 2 1 0-1 --mem 612M --time 0-00:02:00
-
--- watch_mem.sh
+++
@@ -1,5 +0,0 @@
-while true
-do
-	cat /proc/23056/status | grep Peak 2>&1 | tee -a memlog.txt
-	sleep 60
-done
